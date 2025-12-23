4c448c9212504fbcd9cb9bb4f154b868e843c320
aluuminite <reberwack@gmail.com>
2025-12-23 16:32:07 +0100

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,1 +1,57 @@
+from gridMath import toX, toY
 
+def checkNeighbours(plants, index):
+    x, y = toX(index), toY(index)
+
+    # Adjacent (straight)
+    if ((x > 1 and plants[index - 1] != " ") or
+        (x < 10 and plants[index + 1] != " ") or
+        (y > 1 and plants[index - 10] != " ") or
+        (y < 10 and plants[index + 10] != " ")):
+        return 5
+
+    # Diagonal nearby
+    elif ((x > 1 and y > 1 and plants[index - 11] != " ") or
+          (x < 10 and y > 1 and plants[index - 9] != " ") or
+          (x > 1 and y < 10 and plants[index + 9] != " ") or
+          (x < 10 and y < 10 and plants[index + 11] != " ")):
+        return 4
+
+    # 2 away straight
+    elif ((x > 2 and plants[index - 2] != " ") or
+          (x < 9 and plants[index + 2] != " ") or
+          (y > 2 and plants[index - 20] != " ") or
+          (y < 9 and plants[index + 20] != " ")):
+        return 3
+
+    # L-shape (3 away in L)
+    elif ((x > 1 and y > 2 and plants[index - 21] != " ") or
+          (x < 10 and y > 2 and plants[index - 19] != " ") or
+          (x > 1 and y < 9 and plants[index + 19] != " ") or
+          (x < 10 and y < 9 and plants[index + 21] != " ") or
+          (x > 2 and y > 1 and plants[index - 12] != " ") or
+          (x < 9 and y > 1 and plants[index - 8] != " ") or
+          (x > 2 and y < 10 and plants[index + 8] != " ") or
+          (x < 9 and y < 10 and plants[index + 12] != " ")):
+        return 2
+
+    # 2 away diagonally
+    elif ((x > 2 and y > 2 and plants[index - 22] != " ") or
+          (x < 9 and y > 2 and plants[index - 18] != " ") or
+          (x > 2 and y < 9 and plants[index + 18] != " ") or
+          (x < 9 and y < 9 and plants[index + 22] != " ")):
+        return 1
+
+    else:
+        return 0
+
+def checkAdjacentP(plants, index):
+    x, y = toX(index), toY(index)
+
+    # Check straight adjacent cells for "P"
+    if ((x > 1 and plants[index - 1] == "P") or
+        (x < 10 and plants[index + 1] == "P") or
+        (y > 1 and plants[index - 10] == "P") or
+        (y < 10 and plants[index + 10] == "P")):
+        return True
+    return False
